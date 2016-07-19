79fa7202624b01376eb647300275be93cf0e506c
Sebastian Rogala <sebrogala@gmail.com>
2016-07-20 00:50:21 +0200

    Add TaskService interface

---
+++ ITaskService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tasker
+{
+    public interface ITaskService
+    {
+        [OperationContract]
+        bool CreateTask(Task t);
+
+        [OperationContract]
+        bool EditTask(Task t);
+
+        [OperationContract]
+        bool FinishTask(int taskId);
+
+        [OperationContract]
+        string ShowTask(int taskId);
+
+        [OperationContract]
+        string GetTask(int priorityId);
+    }
+
+    [DataContract]
+    public class Task
+    {
+        [DataMember]
+        int id { get; set; }
+
+        [DataMember]
+        string name { get; set; }
+
+        [DataMember]
+        string description { get; set; }
+
+        [DataMember]
+        int priority { get; set; }
+
+        [DataMember]
+        DateTime createDate { get; set; }
+
+        [DataMember]
+        DateTime deadlineDate { get; set; }
+        
+        bool isFinished = false;
+
+        [DataMember]
+        public bool IsFinished
+        {
+            get { return isFinished; }
+            set { isFinished = value; }
+        }
+    }
+}
---
+++ TaskService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tasker
+{
+    public class TaskService : ITaskService
+    {
+
+        SqlConnection connection;
+        SqlCommand command;
+        SqlConnectionStringBuilder connectionStringBuilder;
+
+        public TaskService()
+        {
+            ConnectToDB();
+        }
+
+        private void ConnectToDB()
+        {
+            connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "PC";
+            connectionStringBuilder.InitialCatalog = "TaskerDB";
+            connectionStringBuilder.IntegratedSecurity = true;
+
+            connection = new SqlConnection(connectionStringBuilder.ToString());
+            command = connection.CreateCommand();
+        }
+        
+        public bool CreateTask(Task t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditTask(Task t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FinishTask(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ShowTask(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTask(int priorityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
