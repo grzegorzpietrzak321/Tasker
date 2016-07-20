f70d4e1c99fc9c905f23c5768f19da388585f8ea
grzesiek <grzesiek@DESKTOP-VVMAVA4>
2016-07-20 22:27:40 +0200

    Added data model and DbSet<Task>

---
+++ TaskerDataModel.cs
@@ -0,0 +1,42 @@
+namespace Tasker
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class TaskerDataModel : DbContext
+    {
+        // Your context has been configured to use a 'TaskerDataModel' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Tasker.TaskerDataModel' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'TaskerDataModel' 
+        // connection string in the application configuration file.
+        public TaskerDataModel()
+            : base("name=TaskerDataModel")
+        {
+
+        }
+            public DbSet<Task> Tasks { get; set; }
+        //TODO dbset
+
+        //TODO connection string
+        
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    
+
+
+
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
