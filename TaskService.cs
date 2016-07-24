14f32f09985bb1e2c6c9ac44b9ac0053c5282474
grzesiek <grzesiek@DESKTOP-VVMAVA4>
2016-07-24 14:06:57 +0200

    SVC added and corrected

--- TaskService.cs
+++ TaskService.cs
@@ -9,52 +9,5 @@ using System.Text;
 
 namespace Tasker
 {
-    public class TaskService : ITaskService
-    {
-
-        SqlConnection connection;
-        SqlCommand command;
-        SqlConnectionStringBuilder connectionStringBuilder;
-
-        public TaskService()
-        {
-            ConnectToDB();
-        }
-
-        private void ConnectToDB()
-        {
-            connectionStringBuilder = new SqlConnectionStringBuilder();
-            connectionStringBuilder.DataSource = "PC";
-            connectionStringBuilder.InitialCatalog = "TaskerDB";
-            connectionStringBuilder.IntegratedSecurity = true;
-
-            connection = new SqlConnection(connectionStringBuilder.ToString());
-            command = connection.CreateCommand();
-        }
-        
-        public bool CreateTask(Task t)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool EditTask(Task t)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool FinishTask(int taskId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string ShowTask(int taskId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetTask(int priorityId)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    
 }
--- TaskerService.svc.cs
+++ TaskerService.svc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -10,7 +11,54 @@ namespace Tasker
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TaskerService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select TaskerService.svc or TaskerService.svc.cs at the Solution Explorer and start debugging.
     public class TaskerService : ITaskerService
-    {
+    { 
+
+            SqlConnection connection;
+            SqlCommand command;
+            SqlConnectionStringBuilder connectionStringBuilder;
+
+            public TaskerService()
+            {
+                ConnectToDB();
+            }
+
+            private void ConnectToDB()
+            {
+                connectionStringBuilder = new SqlConnectionStringBuilder();
+                connectionStringBuilder.DataSource = "PC";
+                connectionStringBuilder.InitialCatalog = "TaskerDB";
+                connectionStringBuilder.IntegratedSecurity = true;
+
+                connection = new SqlConnection(connectionStringBuilder.ToString());
+                command = connection.CreateCommand();
+            }
+
+            public bool CreateTask(Task t)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool EditTask(Task t)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool FinishTask(int taskId)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string ShowTask(int taskId)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string GetTask(int priorityId)
+            {
+                throw new NotImplementedException();
+            }
+        
+
         public void DoWork()
         {
         }
